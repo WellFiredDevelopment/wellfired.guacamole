362432329a26f5d272ef7fbe7aaa31faca803477
terry <terrymagnusdrever@wellfired.com>
2015-12-20 17:25:44 +0900

    Added a basic Colour class.

---
+++ Types/UIColor.cs
@@ -0,0 +1,18 @@
+﻿namespace WellFired.Guacamole
+{
+	public partial class UIColor
+	{
+		public float R { get; set; }
+		public float G { get; set; }
+		public float B { get; set; }
+
+		public static UIColor FromRGB(int red, int green, int blue)
+		{
+			return new UIColor {
+				R = (float)red / 255.0f,
+				G = (float)green / 255.0f,
+				B = (float)blue / 255.0f,
+			};
+		}
+	}
+}
