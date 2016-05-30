d9d31fd8c161109b9b075008a7cccc4462913532
Terry Drever <terrymagnusdrever@wellfired.com>
2016-05-30 14:33:13 +0100

    Pixel data structure.

---
+++ Drawing/Pixel.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace WellFired.Guacamole.Drawing
+{
+	public struct Pixel
+	{
+		public int X;
+		public int Y;
+		public UIColor Color;
+
+		public override bool Equals(object obj)
+		{
+			var compareTo = (Pixel)obj;
+			return compareTo.X == X && compareTo.Y == Y && compareTo.Color == Color;
+		}
+
+		public override int GetHashCode()
+		{
+			return X ^ Y ^ Color.GetHashCode();
+		}
+
+		public static bool operator ==(Pixel a, Pixel b)
+		{
+			if (Object.ReferenceEquals(a, b))
+				return true;
+
+			if (((object)a == null) || ((object)b == null))
+				return false;
+
+			return a.Equals(b);
+		}
+
+		public static bool operator !=(Pixel a, Pixel b)
+		{
+			return !(a == b);
+		}
+	}
+}
