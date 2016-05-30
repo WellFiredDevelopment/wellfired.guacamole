d421ebb14120e2b77da878ecb62b8c79d4792d8d
Terry Drever <terrymagnusdrever@wellfired.com>
2016-05-30 15:58:56 +0100

    Corner Mask added so we can define which corners to give a radius.

---
+++ Types/CornerMask.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace WellFired.Guacamole
+{
+	[Flags]
+	public enum CornerMask
+	{
+		TopLeft = 1 << 0,
+		TopRight = 1 << 1,
+		BottomLeft = 1 << 2,
+		BottomRight = 1 << 3,
+
+		All = TopLeft | TopRight | BottomLeft | BottomRight,
+		Right = TopRight | BottomRight,
+		Left = TopLeft | BottomLeft,
+		Top = TopLeft | TopRight,
+		Bottom = BottomLeft | BottomRight
+	}
+}
