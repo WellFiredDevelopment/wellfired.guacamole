f946bdd8bf94fe559eb83699e986692ee876685c
Terry Drever <terrymagnusdrever@wellfired.com>
2016-08-07 19:47:00 +0100

    Add a simple Unit Test Assembly. Doesn't do anything meaningful. Commit all csproj / sln files.

---
+++ Solution/WellFired.Guacamole.Test/UnityTest.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using UnityEngine;
+
+namespace WellFired.Guacamole.Test
+{
+    [TestClass]
+    public class UnityTest
+    {
+        [TestMethod]
+        public void VectorTest()
+        {
+            var vector1 = Vector3.one;
+            var vector2 = Vector3.one;
+            var vector3 = vector1 + vector2;
+            Assert.AreEqual(vector3, new Vector3(2.0f, 2.0f, 2.0f));
+        }
+
+        [TestMethod]
+        public void VectorTest2()
+        {
+            var vector1 = Vector3.one;
+            var vector2 = Vector3.one;
+            var vector3 = vector1 + vector2;
+            Assert.AreNotEqual(vector3, new Vector3(3.0f, 2.0f, 2.0f));
+        }
+    }
+}
