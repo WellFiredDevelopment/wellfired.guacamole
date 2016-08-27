1717d4d2b24f63a62b964e4f7f16540294cdbe6a
Terry Drever <terrymagnusdrever@wellfired.com>
2016-08-27 17:19:15 +0100

    Added an AutoAction.

---
+++ Solution/WellFired.Guacamole.Unity.Runtime/DispsableMonoBehaviour.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using UnityEngine;
+using WellFired.Guacamole.Annotations;
+
+namespace WellFired.Guacamole.Unity.Runtime
+{
+    public abstract class DispsableMonoBehaviour : MonoBehaviour, IDisposable
+    {
+        private bool _disposed;
+        private Disposable _disposable;
+
+        [UsedImplicitly]
+        public virtual void Awake()
+        {
+            _disposable = new Disposable();
+        }
+
+        [PublicAPI]
+        public void Dispose()
+        {
+            if (_disposed)
+                return;
+
+            _disposed = true;
+	        _disposable.Dispose();
+            OnDispose();
+            Destroy(gameObject);
+        }
+
+        [UsedImplicitly]
+        public void OnDestroy()
+        {
+            Dispose();
+        }
+
+        protected virtual void OnDispose()
+        {
+        }
+
+        void IDisposable.AddDisposedCallback(Action action)
+        {
+            _disposable.AddDisposedCallback(action);
+        }
+
+        [PublicAPI]
+        protected void DisposeOf(params System.IDisposable[] disposables)
+        {
+            foreach (var disposable in disposables)
+                disposable?.Dispose();
+        }
+    }
+}
---
+++ Solution/WellFired.Guacamole/Disposable.cs
@@ -0,0 +1,45 @@
+using System;
+using System.Collections.Generic;
+
+namespace WellFired.Guacamole
+{
+	public interface IDisposable : System.IDisposable
+	{
+		void AddDisposedCallback(Action action);
+	}
+
+	public class Disposable : IDisposable
+	{
+		private readonly List<Action> _onDisposed;
+
+		public Disposable()
+		{
+			_onDisposed = new List<Action>();
+		}
+
+		private bool IsDisposed { get; set; }
+
+		public void AddDisposedCallback(Action action)
+		{
+			if (IsDisposed)
+				throw new InvalidOperationException("You are adding an action to an already disposed Disposable.");
+
+			_onDisposed.Add(action);
+		}
+
+		public void Dispose()
+		{
+			if (IsDisposed)
+				return;
+
+			IsDisposed = true;
+
+			var l = _onDisposed.ToArray();
+
+			_onDisposed.Clear();
+			
+			foreach (var a in l)
+				a?.Invoke();
+		}
+	}
+}
