fc5fd2c20fd5eca790118f046f1060bbdcf4d70f
Terry Magnus Drever <terrymagnusdrever@wellfired.com>
2017-01-15 20:24:29 +0000

    Ensure the Children has an empty list on launch.

--- Solution/WellFired.Guacamole/Views/ItemsView.cs
+++ Solution/WellFired.Guacamole/Views/ItemsView.cs
@@ -6,7 +6,7 @@ namespace WellFired.Guacamole.Views
 {
     public abstract partial class ItemsView : View, ICanLayout
     {
-        public IList<ILayoutable> Children { get; set; }
+        public IList<ILayoutable> Children { get; set; } = new List<ILayoutable>();
 
         public void DoLayout()
         {
@@ -19,10 +19,9 @@ namespace WellFired.Guacamole.Views
             foreach (var item in ItemSource)
             {
                 // TODO : Create the object and Set The Binding Context.
-
-                //var view = Activator.CreateInstance(template) as View;
+                var view = Activator.CreateInstance(template) as View;
                 //view.BindingContext = item;
-                //Children.Add(view);
+                Children.Add(view);
             }
         }
     }
