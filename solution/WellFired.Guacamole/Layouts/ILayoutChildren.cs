a98142d9cc8b0ef66c22fdca22fb879e2ab7389e
Terry Magnus Drever <terrymagnusdrever@wellfired.com>
2017-07-09 15:39:00 +0100

    Wrote some useful comments on the order of layouting for new developers.

--- solution/WellFired.Guacamole/Layouts/ILayoutChildren.cs
+++ solution/WellFired.Guacamole/Layouts/ILayoutChildren.cs
@@ -3,10 +3,39 @@ using WellFired.Guacamole.Types;
 
 namespace WellFired.Guacamole.Layouts
 {
+    /// <summary>
+    /// Layouting is a three step process.
+    /// 1. We work out what size we want to be.
+    /// 2. We attempt to fullfil our rect request, though this may not be possible.
+    /// 3. We layout our children in the space that was available to us.
+    /// Each of these steps are called by something and will be called in the correct order.
+    /// </summary>
     public interface ILayoutChildren
     {
-        void Layout(IEnumerable<ILayoutable> layoutables, UIRect availableSpace, UIPadding containerPadding);
+        /// <summary>
+        /// 1. We work out what size we want to be.
+        /// </summary>
+        /// <param name="layoutables"></param>
+        /// <param name="minSize"></param>
+        /// <returns></returns>
         UIRect CalculateValidRectRequest(IEnumerable<ILayoutable> layoutables, UISize minSize);
+        
+        /// <summary>
+        /// 2. We attempt to fullfil our rect request, though this may not be possible.
+        /// </summary>
+        /// <param name="children"></param>
+        /// <param name="availableSpace"></param>
+        /// <param name="containerPadding"></param>
+        /// <param name="horizontalLayout"></param>
+        /// <param name="verticalLayout"></param>
         void AttemptToFullfillRequests(IList<ILayoutable> children, UIRect availableSpace, UIPadding containerPadding, LayoutOptions horizontalLayout, LayoutOptions verticalLayout);
+        
+        /// <summary>
+        /// 3. We layout our children in the space that was available to us.
+        /// </summary>
+        /// <param name="layoutables"></param>
+        /// <param name="availableSpace"></param>
+        /// <param name="containerPadding"></param>
+        void Layout(IEnumerable<ILayoutable> layoutables, UIRect availableSpace, UIPadding containerPadding);
     }
 }
