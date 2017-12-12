2b5fde23eca48b829a8abe7454064b2637400731
Thibault Gouala <thibault@drpanda.com>
2017-12-12 16:55:31 +0800

    Finish FilterView

---
+++ solution/Example/WellFired.Guacamole.Examples/Simple/FilterExample/FilterTestWindow.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel;
+using WellFired.Guacamole.Data;
+using WellFired.Guacamole.DataBinding;
+using WellFired.Guacamole.Diagnostics;
+using WellFired.Guacamole.Layouts;
+using WellFired.Guacamole.Platform;
+using WellFired.Guacamole.Views;
+
+namespace WellFired.Guacamole.Examples.Simple.FilterExample
+{
+	public class FilterTestWindow : Window
+	{
+		public FilterTestWindow(ILogger logger, INotifyPropertyChanged persistantData, IPlatformProvider platformProvider) : base(logger, persistantData, platformProvider)
+		{
+			var introLabel = new Label {
+				Text = "Filter is a Text Entry that process the input to extract a list of words, and" +
+				       " key/value pairs entered by the user. For example, \"sausage, guacamole t:prefab count:3\", will return" +
+				       " a list {sausage, guacamole} and a dictionary {{t, prefab}, {count, 3}}.",
+				Padding = UIPadding.Of(5)
+			};
+			var filter = new FilterView{Padding = UIPadding.Of(5)};
+			var label = new Label{Padding = UIPadding.Of(5)};
+Padding = UIPadding.Of(30);
+			Content = LayoutView.WithAdjacentVertical(new List<ILayoutable> {introLabel, filter, label});
+			
+			BindingContext = new ViewModel();
+			
+			filter.Bind(FilterView.SimpleSearchProperty, "SimpleSearch", BindingMode.TwoWay);
+			filter.Bind(FilterView.KeyValueSearchProperty, "KeyValueSearch", BindingMode.TwoWay);
+			label.Bind(Label.TextProperty, "Text", BindingMode.OneWay);
+		}
+	}
+}
---
+++ solution/Example/WellFired.Guacamole.Examples/Simple/FilterExample/ViewModel.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using WellFired.Guacamole.DataBinding;
+
+namespace WellFired.Guacamole.Examples.Simple.FilterExample
+{
+	public class ViewModel : NotifyBase
+	{
+		private List<string> _simpleSearch;
+		private Dictionary<string, string> _keyValueSearch;
+		private string _text;
+
+		public List<string> SimpleSearch
+		{
+			get => _simpleSearch;
+			set
+			{
+				SetProperty(ref _simpleSearch, value);
+				UpdateText();
+			}
+		}
+
+		public Dictionary<string, string> KeyValueSearch
+		{
+			get => _keyValueSearch;
+			set
+			{
+				SetProperty(ref _keyValueSearch, value);
+				UpdateText();
+			}
+		}
+
+		public string Text
+		{
+			get => _text;
+			set => SetProperty(ref _text, value);
+		}
+
+		private void UpdateText()
+		{
+			var keyValueSearches = _keyValueSearch != null
+				? string.Concat(_keyValueSearch.Select(keyValuePair => $"{{{keyValuePair.Key} : {keyValuePair.Value}}} ").ToArray())
+				: "";
+
+			var simpleSearch = _simpleSearch != null
+				? string.Concat(_simpleSearch.Select(search => $"{search} ").ToArray())
+				: "";
+			
+			Text = $"Key/Value searches : {keyValueSearches}\nSimple Search : {simpleSearch}";
+		}
+	}
+}
