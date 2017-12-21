97620915cf7c2a26c0725f9f1c70baff183c4eba
Thibault Gouala <thibault@drpanda.com>
2017-12-21 17:58:50 +0800

    Finish Add a logger writing in the console

---
+++ solution/WellFired.Guacamole/Diagnostics/ConsoleLogger.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace WellFired.Guacamole.Diagnostics
+{
+	public class ConsoleLogger : ILogger
+	{
+		public void LogMessage(string message)
+		{
+			Console.WriteLine(message);
+		}
+
+		public void LogWarning(string message)
+		{
+			Console.WriteLine(message);
+		}
+
+		public void LogError(string message)
+		{
+			Console.WriteLine(message);
+		}
+	}
+}
