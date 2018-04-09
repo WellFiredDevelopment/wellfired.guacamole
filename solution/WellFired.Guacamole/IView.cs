5e87a240cced156bae2c547b46a35ea7f6d38a6f
Thibault Gouala <thibault@drpanda.com>
2018-04-09 11:12:55 +0800

    Add useful doc

--- solution/WellFired.Guacamole/IView.cs
+++ solution/WellFired.Guacamole/IView.cs
@@ -8,9 +8,22 @@ namespace WellFired.Guacamole
 	{
 	    IView Content { get; }
 	    UIPadding Padding { get; }
+		/// <summary>
+		/// In one rendering loop, this value will be the size and position that the view is requesting, then the size and position
+		/// that could be assigned to it based on the available space.
+		/// </summary>
 	    UIRect RectRequest { get; set; }
 		UIRect ContentRectRequest { get; set; }
+		/// <summary>
+		/// This is the id of the view. Most algorithm using it will consider this value to be unique for each views.
+		/// So it is recommended to assign it with a random and unique fashion (GUID, incremental value), or with an arbitrary value for
+		/// debugging purpose.
+		/// </summary>
 	    string Id { get; set; }
+		/// <summary>
+		/// Flag to determine if <see cref="RectRequest"/> should be recalculated or not. This is used for optimization purpose to avoid recalculating
+		/// the size requested by a view if there is no reason for it to have changed.
+		/// </summary>
 	    bool ValidRectRequest { get; set; }
 	    INativeRenderer NativeRenderer { get; }
 	    UISize MinSize { get; }
