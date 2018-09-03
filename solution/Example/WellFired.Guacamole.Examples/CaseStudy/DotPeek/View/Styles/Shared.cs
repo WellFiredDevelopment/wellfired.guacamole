03fcfbca9dc4a6a674aa2ed26874ab544f566980
Thibault Gouala <thibault@drpanda.com>
2018-09-03 18:21:05 +0200

    Remove DotPeek

--- solution/Example/WellFired.Guacamole.Examples/CaseStudy/DotPeek/View/Styles/Shared.cs
+++
@@ -1,9 +0,0 @@
-﻿using WellFired.Guacamole.Data;
-
-namespace WellFired.Guacamole.Examples.CaseStudy.DotPeek.View.Styles
-{
-	public static class Shared
-	{
-		public static UIColor BackgroundColor = UIColor.FromRGB(40, 40, 40);
-	}
-}
